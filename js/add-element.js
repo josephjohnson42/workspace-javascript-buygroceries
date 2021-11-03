c589b84d997df3eca051933a6ddf078d366ea2b5
josephjohnson42 <jmj18@usca.edu>
2021-11-03 12:39:21 -0400

    changeStyle

--- js/add-element.js
+++ js/add-element.js
@@ -3,6 +3,10 @@
  */
 
 // get all the hot class elements- change them to cool class
+$('.hot').each(function () {
+  $(this).removeClass('hot');
+  $(this).addClass('cool');
+});
 
 // traverse the elements
 
@@ -14,16 +18,22 @@
 function addElement() {
   // add a new element
   // add a input text box
-  
   // whenever the user are done add the element
-  
 }
 
 // bind click with the event handler
-
+$('li').click(changeStyle);
 //  click the li element will change the changeStyle
 //  three style : complete, cool, hot
-function changeStyle() {}
+function changeStyle() {
+  if ($(this).hasClass('cool')) {
+    $(this).removeClass('cool');
+    $(this).addClass('complete');
+  } else if ($(this).hasClass('complete')) {
+    $(this).removeClass('complete');
+    $(this).addClass('cool');
+  }
+}
 
 // delete complete element by clicking the trash can
 document.getElementById('remove').addEventListener('click', removeElement);
